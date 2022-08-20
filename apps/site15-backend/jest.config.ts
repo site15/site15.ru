f4c85cd2e11dc37a383742ad5a06ef12d08e2a71
ArgoN1ck <jsargenbagyshanov@gmail.com>
2022-08-20 22:08:21 +0600

    feat: create backend app

---
+++ apps/site15-backend/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'site15-backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/site15-backend',
+};
