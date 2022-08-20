d02f4e332adae05af548909ef86a9903a40859f5
ArgoN1ck <jsargenbagyshanov@gmail.com>
2022-08-21 00:14:06 +0600

    create frontend app

---
+++ apps/site15-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'site15.ru-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'site15-frontend';
+}
