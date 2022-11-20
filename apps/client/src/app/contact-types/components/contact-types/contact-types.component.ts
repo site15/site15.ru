d842b63ea2444d809890246cda256e6fd8fb09f3
ArgoN1ck <jsargenbagyshanov@gmail.com>
2022-11-20 12:36:36 +0600

    feat(client): unsubscribe from observable & crud with backend #23

--- apps/client/src/app/contact-types/components/contact-types/contact-types.component.ts
+++ apps/client/src/app/contact-types/components/contact-types/contact-types.component.ts
@@ -1,34 +1,81 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnDestroy,
-  OnInit,
-} from "@angular/core";
-import { Observable } from "rxjs";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { Subject, tap } from "rxjs";
+
 import { IContactTypes } from "../../../shared/models/contact-types.model";
 import { ContactTypesService } from "../../contact-types.service";
 
+@UntilDestroy()
 @Component({
   selector: "site15-contact-types",
   templateUrl: "./contact-types.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactTypesComponent implements OnInit, OnDestroy {
-  contactTypes$!: Observable<IContactTypes[]>;
+export class ContactTypesComponent implements OnInit {
+  contactTypes$ = new Subject<IContactTypes[]>();
   contactType!: IContactTypes;
   contactTypesDialog!: boolean;
 
+  /* UI property */
   editing!: boolean;
 
   constructor(private contactTypesService: ContactTypesService) {}
 
   ngOnInit(): void {
-    this.contactTypes$ = this.contactTypesService.getAllContactTypes();
+    this.getContactTypes();
   }
 
-  ngOnDestroy(): void {
-    console.log("Here will be added unsubscribe functions :)");
+  getContactTypes() {
+    this.contactTypesService
+      .getAllContactTypes()
+      .pipe(
+        tap((items) => this.contactTypes$.next(items)),
+        untilDestroyed(this)
+      )
+      .subscribe();
   }
+
+  deleteContactType(id: number) {
+    this.contactTypesService
+      .deleteContactType(id)
+      .pipe(
+        tap(() => {
+          this.getContactTypes();
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe();
+  }
+
+  createContactType(ct: IContactTypes) {
+    this.contactTypesService
+      .createContactType(ct)
+      .pipe(
+        tap(() => {
+          this.getContactTypes();
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe();
+    this.contactTypesDialog = false;
+  }
+
+  saveContactType(ct: IContactTypes) {
+    this.contactTypesService
+      .updateContactType(ct)
+      .pipe(
+        tap(() => {
+          this.getContactTypes();
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe();
+    this.contactTypesDialog = false;
+  }
+
+  /**
+   * UI methods
+   */
 
   hideDialog() {
     this.contactTypesDialog = false;
@@ -40,28 +87,9 @@ export class ContactTypesComponent implements OnInit, OnDestroy {
     this.editing = false;
   }
 
-  deleteContactType(id: number) {
-    this.contactTypesService.deleteContactType(id).subscribe();
-  }
-
-  createContactType(ct: IContactTypes) {
-    this.contactTypesService
-      .createContactType(ct)
-      .subscribe(
-        () =>
-          (this.contactTypes$ = this.contactTypesService.getAllContactTypes())
-      );
-    this.contactTypesDialog = false;
-  }
-
   editContactType(ct: IContactTypes) {
     this.contactType = { ...ct };
     this.contactTypesDialog = true;
     this.editing = true;
-  }
-
-  saveContactType(ct: IContactTypes) {
-    this.contactTypesService.updateContactType(ct).subscribe();
-    this.contactTypesDialog = false;
   }
 }
