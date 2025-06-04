a3e3923481d58c694c8a10fb00c139f8a6220385
EndyKaufman <admin@site15.ru>
2025-06-05 00:41:11 +0500

    chore: remove libraries from patcher for new prisma client and remove nx libraries from tsconfig

--- fix-new-clients.ts
+++ fix-new-clients.ts
@@ -1,10 +1,7 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 
 const files2: string[] = [
-  'libs/feature/notifications/src/lib/generated/prisma-client/internal/class.ts',
   'libs/feature/sso/src/lib/generated/prisma-client/internal/class.ts',
-  'libs/feature/two-factor/src/lib/generated/prisma-client/internal/class.ts',
-  'libs/feature/webhook/src/lib/generated/prisma-client/internal/class.ts',
 ];
 for (let file of files2) {
   writeFileSync(
